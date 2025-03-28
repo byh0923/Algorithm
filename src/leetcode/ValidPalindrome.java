c62c0410e8758941c5e105493f20deda43c65f25
배영현 <byh2949@gmail.com>
2025-03-28 23:56:59 +0900

    valid palindrome / merge sorted array
    투포인터문제..

---
+++ src/leetcode/ValidPalindrome.java
@@ -0,0 +1,39 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidPalindrome {
+    public static boolean isPalindrome(String s) {
+        boolean result = true;
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+
+        }
+
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s));
+    }
+}
